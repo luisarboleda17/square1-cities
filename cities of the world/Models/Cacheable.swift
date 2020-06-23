669d7c2cdfe8a0f31bd8d0b87a9ee21b3ee37d2b
luis_arboleda17 <hello@larboleda.io>
2020-06-23 17:25:20 -0500

    Cacheable expiry date converted to optional

--- cities of the world/Models/Cacheable.swift
+++ cities of the world/Models/Cacheable.swift
@@ -10,6 +10,6 @@ import Foundation
 import RealmSwift
 
 class Cacheable: Object {
-    @objc dynamic var expiryDate: Date!
+    @objc dynamic var expiryDate: Date?
     @objc dynamic var query: String!
 }
