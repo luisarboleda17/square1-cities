4dfb1f9b43753d6328ae2488642c6d3bafe05416
luis_arboleda17 <hello@larboleda.io>
2020-06-23 14:39:11 -0500

    Cacheable base model created

---
+++ cities of the world/Models/Cacheable.swift
@@ -0,0 +1,15 @@
+//
+//  Cacheable.swift
+//  cities of the world
+//
+//  Created by Luis Arboleda on 6/23/20.
+//  Copyright © 2020 Luis Arboleda. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Cacheable: Object {
+    @objc dynamic var limitValidDate: Date?
+    @objc dynamic var query: String?
+}
