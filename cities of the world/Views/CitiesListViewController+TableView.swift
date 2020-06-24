198c086cd2d2cea7bc12551aa20fc09c108de563
luis_arboleda17 <hello@larboleda.io>
2020-06-24 18:00:35 -0500

    Added example value to cities table data source

---
+++ cities of the world/Views/CitiesListViewController+TableView.swift
@@ -0,0 +1,25 @@
+//
+//  CitiesListViewController+TableView.swift
+//  cities of the world
+//
+//  Created by Luis Arboleda on 6/24/20.
+//  Copyright © 2020 Luis Arboleda. All rights reserved.
+//
+
+import UIKit
+
+extension CitiesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension CitiesListViewController: UITableViewDelegate {
+    
+}
