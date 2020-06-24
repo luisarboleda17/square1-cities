eb550792e2e8ca1403f4aa9e444596b555b595d6
luis_arboleda17 <hello@larboleda.io>
2020-06-23 23:44:09 -0500

    Cities repository created

---
+++ cities of the world/Repositories/CitiesRepository.swift
@@ -0,0 +1,48 @@
+//
+//  CitiesRepository.swift
+//  cities of the world
+//
+//  Created by Luis Arboleda on 6/23/20.
+//  Copyright © 2020 Luis Arboleda. All rights reserved.
+//
+
+import Foundation
+
+class CitiesRepository {
+    private let apiClient: ApiClient!
+    private let cacheManager: CacheManager!
+    
+    required init(apiClient: ApiClient, cacheManager: CacheManager) {
+        self.apiClient = apiClient
+        self.cacheManager = cacheManager
+    }
+    
+    public func search(withQuery query: String, page: Int, completion: @escaping (Array<City>) -> Void) {
+        if let results = cacheManager.getResults(forQuery: query, objectType: City.self),
+            results.count > 0 {
+            print("Cargo de caché")
+            completion(Array(results))
+        } else {
+            apiClient.searchCities(withQuery: query, page: page) {
+                response in
+                if let response = response {
+                    print(response)
+                    print("Cargo de internet")
+                    
+                    let cities: Array<City> = response.data.items.map{
+                        city in
+                        city.page = page
+                        city.query = query
+                        city.expiryDate = Date().addingTimeInterval(3600) // 1 hour cache
+                        return city
+                    }
+                    self.cacheManager.addElements(elements: cities, objectType: City.self)
+                    completion(cities)
+                } else {
+                    print("No hay nada en internet")
+                }
+            }
+        }
+    }
+    
+}
