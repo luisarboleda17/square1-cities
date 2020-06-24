3be962a01aa65ea6c7bcc2d495c872cd3a918895
luis_arboleda17 <hello@larboleda.io>
2020-06-23 19:02:01 -0500

    Valid in memory cache test created

--- cities of the worldTests/CacheManagerTests.swift
+++ cities of the worldTests/CacheManagerTests.swift
@@ -96,11 +96,73 @@ class CacheManagerTests: XCTestCase {
         if let results = results {
             XCTAssert(results.count == 2)
         }
-        
     }
     
     func testGetResultsValidMemoryCache() throws {
-
+        try clearDatabase()
+        
+        // Create realms
+        let inMemoryRealm = try Realm(configuration: inMemoryConfiguration)
+        let storageRealm = try Realm(configuration: storeConfiguration)
+        let query = "panama"
+        
+        // Add in memory default values
+        try inMemoryRealm.write {
+            inMemoryRealm.add(
+                City(
+                    id: 1,
+                    name: "Panama",
+                    localName: "Panama",
+                    lat: nil,
+                    lng: nil,
+                    updatedAt: Date(),
+                    countryName: "Panama",
+                    continentName: "America",
+                    query: query,
+                    expiryDate: Date().addingTimeInterval(5 * 60) // 5 minutes before -> Expired
+                )
+            )
+        }
+        
+        // Add storage default values
+        try storageRealm.write {
+            storageRealm.add(
+                City(
+                    id: 1,
+                    name: "Panama",
+                    localName: "Panama",
+                    lat: nil,
+                    lng: nil,
+                    updatedAt: Date(),
+                    countryName: "Panama",
+                    continentName: "America",
+                    query: query,
+                    expiryDate: Date().addingTimeInterval(5 * 60) // 5 minutes after -> Not Expired
+                )
+            )
+            storageRealm.add(
+                City(
+                    id: 2,
+                    name: "Bogota",
+                    localName: "Bogota",
+                    lat: nil,
+                    lng: nil,
+                    updatedAt: Date(),
+                    countryName: "Colombia",
+                    continentName: "America",
+                    query: query,
+                    expiryDate: Date().addingTimeInterval(5 * 60) // 5 minutes after -> Not Expired
+                )
+            )
+        }
+        
+        // Validate
+        let results = cacheManager.getResults(forQuery: query, objectType: City.self)
+        XCTAssertNotNil(results)
+        
+        if let results = results {
+            XCTAssert(results.count == 1, "\(results.count) results")
+        }
     }
     
     func testGetResultsInvalidStorageCache() throws {
