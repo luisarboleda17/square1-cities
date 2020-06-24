bf03b47c6010f8db16494fb4fd463b4286e0e8e6
luis_arboleda17 <hello@larboleda.io>
2020-06-24 10:26:08 -0500

    Added page comparission to search query

--- cities of the world/Repositories/CitiesRepository.swift
+++ cities of the world/Repositories/CitiesRepository.swift
@@ -12,13 +12,15 @@ class CitiesRepository {
     private let apiClient: ApiClient!
     private let cacheManager: CacheManager!
     
+    private let DEFAULT_CACHE_TTL: TimeInterval = 3600 // 1 hour cache
+    
     required init(apiClient: ApiClient, cacheManager: CacheManager) {
         self.apiClient = apiClient
         self.cacheManager = cacheManager
     }
     
     public func search(withQuery query: String, page: Int, completion: @escaping (Array<City>) -> Void) {
-        if let results = cacheManager.getResults(forQuery: query, objectType: City.self),
+        if let results = cacheManager.getResults(forQuery: query, page: page, objectType: City.self),
             results.count > 0 {
             print("Cargo de caché")
             completion(Array(results))
@@ -33,7 +35,7 @@ class CitiesRepository {
                         city in
                         city.page = page
                         city.query = query
-                        city.expiryDate = Date().addingTimeInterval(3600) // 1 hour cache
+                        city.expiryDate = Date().addingTimeInterval(self.DEFAULT_CACHE_TTL)
                         return city
                     }
                     self.cacheManager.addElements(elements: cities, objectType: City.self)
