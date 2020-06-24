c193e746d7100e306af5dd08b51fa7aaa0d42936
luis_arboleda17 <hello@larboleda.io>
2020-06-23 21:39:07 -0500

    Pagination result model created

---
+++ cities of the world/Models/PaginationResult.swift
@@ -0,0 +1,16 @@
+//
+//  PaginationResult.swift
+//  cities of the world
+//
+//  Created by Luis Arboleda on 6/23/20.
+//  Copyright © 2020 Luis Arboleda. All rights reserved.
+//
+
+import Foundation
+
+struct PaginationResult {
+    let currentPage: Int
+    let lastPage: Int
+    let perPage: Int
+    let total: Int
+}
