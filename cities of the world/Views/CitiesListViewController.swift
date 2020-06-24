4ebf8595ba19d03c8da2ad964c5d87cdce06aaa1
luis_arboleda17 <hello@larboleda.io>
2020-06-24 14:22:07 -0500

    Basic Cities ListView created

--- cities of the world/Views/CitiesListViewController.swift
+++ cities of the world/Views/CitiesListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  CitiesListViewController.swift
+//  TestViewController.swift
 //  cities of the world
 //
 //  Created by Luis Arboleda on 6/24/20.
@@ -9,11 +9,30 @@
 import UIKit
 
 protocol CitiesListViewDelegateProtocol {
+    
 }
 
-class CitiesListViewController: UIViewController & BindableViewDelegate & CitiesListViewDelegateProtocol {
-    typealias VM = CitiesListViewModelProtocol
+class CitiesListViewController<VM: CitiesListViewModelProtocol & BindableViewModel>: UIViewController & BindableViewDelegate & CitiesListViewDelegateProtocol {
+    typealias ViewModel = VM
+
+    internal var viewModel: ViewModel!
     
-    internal var viewModel: VM!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Initi the view")
+        // Do any additional setup after loading the view.
+    }
     
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
