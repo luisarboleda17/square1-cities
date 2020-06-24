b689d909dc3e888e1877e78a907367ba71c49842
luis_arboleda17 <hello@larboleda.io>
2020-06-24 10:50:11 -0500

    Initial cities list view created

---
+++ cities of the world/Views/CitiesListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CitiesListViewController.swift
+//  cities of the world
+//
+//  Created by Luis Arboleda on 6/24/20.
+//  Copyright © 2020 Luis Arboleda. All rights reserved.
+//
+
+import UIKit
+
+class CitiesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
