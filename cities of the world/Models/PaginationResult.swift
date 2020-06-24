d6aa8f8ac06db3d7ea3d093e58982cfa99633d6b
luis_arboleda17 <hello@larboleda.io>
2020-06-23 21:39:29 -0500

    Added decodable protocol to pagination result model

--- cities of the world/Models/PaginationResult.swift
+++ cities of the world/Models/PaginationResult.swift
@@ -14,3 +14,23 @@ struct PaginationResult {
     let perPage: Int
     let total: Int
 }
+
+extension PaginationResult: Decodable {
+    enum ResultKeys: String, CodingKey {
+        case currentPage = "current_page"
+        case lastPage = "last_page"
+        case perPage = "per_page"
+        case total = "total"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: ResultKeys.self)
+        
+        self.init(
+            currentPage: try container.decode(Int.self, forKey: .currentPage),
+            lastPage: try container.decode(Int.self, forKey: .lastPage),
+            perPage: try container.decode(Int.self, forKey: .perPage),
+            total: try container.decode(Int.self, forKey: .total)
+        )
+    }
+}
