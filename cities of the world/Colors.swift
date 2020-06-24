e01303cf083eefea66703cade36459050d026cc6
luis_arboleda17 <hello@larboleda.io>
2020-06-24 17:27:48 -0500

    Added colors, constants and search textfield style

---
+++ cities of the world/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  cities of the world
+//
+//  Created by Luis Arboleda on 6/24/20.
+//  Copyright © 2020 Luis Arboleda. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let backgroundGrey = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1.0)
+}
---
+++ cities of the world/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  cities of the world
+//
+//  Created by Luis Arboleda on 6/24/20.
+//  Copyright © 2020 Luis Arboleda. All rights reserved.
+//
+
+import Foundation
+
+let DEFAULT_BORDER_RADIUS = 8.0
