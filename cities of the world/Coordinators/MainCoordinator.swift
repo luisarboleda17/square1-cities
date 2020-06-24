6d76605000b98cc89962011ec33527a508a5ff8c
luis_arboleda17 <hello@larboleda.io>
2020-06-24 11:22:26 -0500

    Base coordinator and view model created

---
+++ cities of the world/Coordinators/MainCoordinator.swift
@@ -0,0 +1,25 @@
+//
+//  MainCoordinator.swift
+//  cities of the world
+//
+//  Created by Luis Arboleda on 6/24/20.
+//  Copyright © 2020 Luis Arboleda. All rights reserved.
+//
+
+import UIKit
+
+class MainCoordinator: Coordinator {
+    internal var navigationController: UINavigationController!
+    
+    required init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        <#code#>
+    }
+    
+    private func loadCitiesList() {
+        
+    }
+}
---
+++ cities of the world/Views/CitiesListViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesListViewModel.swift
+//  cities of the world
+//
+//  Created by Luis Arboleda on 6/24/20.
+//  Copyright © 2020 Luis Arboleda. All rights reserved.
+//
+
+import Foundation
+
+protocol CitiesListViewModelProtocol {
+    
+}
+
+class CitiesListViewModel: CitiesListViewModelProtocol {
+    
+}
