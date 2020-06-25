8a099a0d2d992d2a6cf5f6dd636ea43f49f2d6d0
luis_arboleda17 <hello@larboleda.io>
2020-06-24 20:09:38 -0500

    Identifiers constants created

---
+++ cities of the world/Identifiers.swift
@@ -0,0 +1,13 @@
+//
+//  Identifiers.swift
+//  cities of the world
+//
+//  Created by Luis Arboleda on 6/24/20.
+//  Copyright © 2020 Luis Arboleda. All rights reserved.
+//
+
+import Foundation
+
+struct Identifiers {
+    static let cityCell = "cityCell"
+}
