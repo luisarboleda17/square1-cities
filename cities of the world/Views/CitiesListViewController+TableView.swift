58dcea975b34a04f72a7dd7eaad970ce679cc94a
luis_arboleda17 <hello@larboleda.io>
2020-06-24 19:01:24 -0500

    Added basic methods for cities data source and delegate

--- cities of the world/Views/CitiesListViewController+TableView.swift
+++ cities of the world/Views/CitiesListViewController+TableView.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 extension CitiesListViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
-    
-    
 }
 
 extension CitiesListViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
