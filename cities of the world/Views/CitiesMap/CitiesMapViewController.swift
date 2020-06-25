22fa83610a0d3090c64d044c19120e99c4872211
luis_arboleda17 <hello@larboleda.io>
2020-06-24 22:46:52 -0500

    Marker adding commented on cities map

--- cities of the world/Views/CitiesMap/CitiesMapViewController.swift
+++ cities of the world/Views/CitiesMap/CitiesMapViewController.swift
@@ -17,26 +17,20 @@ class CitiesMapViewController: UIViewController & BindableViewDelegate & CitiesM
     typealias ViewModel = CitiesMapViewModelProtocol
     
     var viewModel: CitiesMapViewModelProtocol!
+    
+    @IBOutlet weak var mapsView: GMSMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
-        self.view.addSubview(mapView)
-
+    }
+    
+    func citiesChanged() {
         // Creates a marker in the center of the map.
+        /*
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
         marker.title = "Sydney"
         marker.snippet = "Australia"
-        marker.map = mapView
-    }
-    
-    func citiesChanged() {
-        
+        marker.map = mapView*/
     }
 }
