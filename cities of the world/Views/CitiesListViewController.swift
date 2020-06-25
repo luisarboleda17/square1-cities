82f3bd24280c08bf2c61d9b31feef80f956a123c
luis_arboleda17 <hello@larboleda.io>
2020-06-24 20:37:40 -0500

    Search textfield actions created

--- cities of the world/Views/CitiesListViewController.swift
+++ cities of the world/Views/CitiesListViewController.swift
@@ -44,7 +44,31 @@ class CitiesListViewController: UIViewController & BindableViewDelegate & Cities
         citiesTableView.register(UINib(nibName: Xibs.cityCell, bundle: Bundle.main), forCellReuseIdentifier: Identifiers.cityCell)
     }
     
-    func citiesChanged() {
+    internal func citiesChanged() {
         self.citiesTableView.reloadData()
+    }
+    
+    
+    @IBAction func searchEditingEnd(_ sender: UITextField) {
+        onSearchSubmitted(query: sender.text)
+    }
+    
+    
+    @IBAction func searchSubmited(_ sender: UITextField) {
+        onSearchSubmitted(query: sender.text)
+    }
+    
+    private func searchQueryIsValid(query: String) -> Bool {
+        guard query.count > 0 else {
+            return false
+        }
+        return true
+    }
+    
+    private func onSearchSubmitted(query: String?) {
+        guard let query = query, searchQueryIsValid(query: query) else {
+            return
+        }
+        viewModel.searchCities(query: query)
     }
 }
