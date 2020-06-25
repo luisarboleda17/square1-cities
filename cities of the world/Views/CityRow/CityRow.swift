c621d2f04e3b98c6fb18b6457160d6071e438af0
luis_arboleda17 <hello@larboleda.io>
2020-06-24 19:10:01 -0500

    City Row created

---
+++ cities of the world/Views/CityRow/CityRow.swift
@@ -0,0 +1,24 @@
+//
+//  CityRow.swift
+//  cities of the world
+//
+//  Created by Luis Arboleda on 6/24/20.
+//  Copyright © 2020 Luis Arboleda. All rights reserved.
+//
+
+import UIKit
+
+class CityRow: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
